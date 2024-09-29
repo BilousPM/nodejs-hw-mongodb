8450f427b50c01d829a60c8633ba4f12de5ec37c
BilousPM <bilouspm@gmail.com>
2024-09-29 17:30:17 -0600

    added Body validation, and ID validation.

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -7,12 +7,29 @@ import {
   updateContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+
 const contactRouter = Router();
+
+contactRouter.use('/:contactId', isValidId('contactId'));
 
 contactRouter.get('/', ctrlWrapper(getAllContactsController));
 contactRouter.get('/:contactId', ctrlWrapper(getContactByIDController));
-contactRouter.post('/', ctrlWrapper(createContactController));
-contactRouter.patch('/:contactId', ctrlWrapper(updateContactController));
+contactRouter.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+contactRouter.patch(
+  '/:contactId',
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
 contactRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default contactRouter;

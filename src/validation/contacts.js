cc80412385cd74ab86621ae75c28d85e0425abc7
BilousPM <bilouspm@gmail.com>
2024-10-01 13:58:10 -0600

    change Validation rules

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number(),
-  email: Joi.string().email().min(3).max(20).required(),
+  phoneNumber: Joi.number().required(),
+  email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .min(3)
